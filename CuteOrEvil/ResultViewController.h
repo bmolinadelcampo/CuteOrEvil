a4c3071780ed098882d0c0c8959e1a8690a1fce4
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-05-07 13:35:11 +0100

    Add result view controller.

---
+++ CuteOrEvil/ResultViewController.h
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.h
+//  CuteOrEvil
+//
+//  Created by Belén Molina del Campo on 07/05/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@end
---
+++ Pods/Target Support Files/Pods-CuteOrEvil/Pods-CuteOrEvil-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CuteOrEvil : NSObject
+@end
+@implementation PodsDummy_Pods_CuteOrEvil
+@end
